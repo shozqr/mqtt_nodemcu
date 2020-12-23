cfe18a42f3c39b0996fc6db92f510c3ce78c877a
shozqr <sabanozgur@gmail.com>
2020-12-23 12:18:36 +0300

    Removed database, added index.html, changed .gitignore file

--- broker/broker.js
+++ broker/broker.js
@@ -2,14 +2,46 @@
 
 const aedes = require('aedes')();
 const server = require('net').createServer(aedes.handle);
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
 const port = 1883;
+const wsPort = 8888;
+
 const sqlite3 = require('sqlite3');
 const open = require('sqlite').open;
+let db;
 
-server.listen(port, function () {
+async function createTable() {
+    try {
+        await db.run(`CREATE TABLE IF NOT EXISTS mqtt_messages(id integer primary key autoincrement, 
+            topic text not null, message text not null, created_at timestamp
+             default (datetime('now', 'localtime')));`);
+
+    } catch (err) {
+        throw err;
+    }
+}
+
+server.listen(port, async function () {
     console.log('server listening on port', port);
+    try {
+        db = await open({
+            filename: 'mqtt.db',
+            driver: sqlite3.Database
+        })
+        await createTable();
+    } catch (err) {
+        console.log(`error: ${err}`);
+    }
 });
 
+ws.createServer({
+    server: httpServer
+}, aedes.handle)
+
+httpServer.listen(wsPort, function () {
+    console.log('websocket server listening on port', wsPort)
+})
 
 aedes.on('clientError', function (client, err) {
     console.log('client error', client.id, err.message, err.stack);
@@ -44,11 +76,3 @@ aedes.on('subscribe', function (subscriptions, client) {
 aedes.on('client', function (client) {
     console.log('new client', client.id);
 });
-
-let db;
-(async () => {
-    db = await open({
-        filename: 'mqtt.db',
-        driver: sqlite3.Database
-    })
-})();
